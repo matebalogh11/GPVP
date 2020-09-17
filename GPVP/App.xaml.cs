fd8c874b6e3936c1d8ae883a76f7d71f0f2a32a9
Máté Balogh <b.mate11@gmail.com>
2020-09-17 12:18:58 +0200

    basic structure created

--- GPVP/App.xaml.cs
+++ GPVP/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GPVP.Properties;
 using System.Windows;
 
 namespace GPVP
@@ -13,5 +8,17 @@ namespace GPVP
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            InitWindow();
+        }
+
+        private void InitWindow()
+        {
+            Current.MainWindow = new Views.MainWindow(new ViewModels.MainWindowModel());
+            Current.MainWindow.Title = Displayresource.AppName;
+            Current.MainWindow.Show();
+        }
     }
 }
